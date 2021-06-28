ba907cabc95a8b60ef7eac046cfce4cc204171fd
Shadi432 <30848732+Shadi432@users.noreply.github.com>
2021-06-29 00:10:03 +0100

    Add files via upload

--- GrappleGame/main.lua
+++ GrappleGame/main.lua
@@ -8,13 +8,22 @@ local function checkCollision(object1,object2)
     end
 end
 
+local function wasVerticallyAlligned(object1,object2)
+    return (object1.y + 50 > object2.y) and (object1.y < object2.y + 50)
+end
+
+local function wasHorizontallyAlligned(object1,object2)
+    return (object1.x + 50 > object2.x) and (object1.x < object2.x + 50)
+end
+
 local function createWall(x,y)
     local wall = {
         image = love.graphics.newImage("Wall.png"),
         x = x,
         y = y,
         sx = 1,
-        sy = 1
+        sy = 1,
+        type = "wall"
     }
 
 
@@ -22,15 +31,40 @@ local function createWall(x,y)
 end
 
 local function resolveCollision(object1,object2)
-    local centre1 = object1.x + 24
-    local centre2 = object2.x + 24
+    local xCentre1 = object1.x + 25 -- 24 is half the width of each tile
+    local xCentre2 = object2.x + 25
 
-    if centre1 >= centre2 then
-        left = false
-        right = true
-    elseif centre1 < centre2 then
-        right = false
-        left = true
+    local yCentre1 = object1.y + 25
+    local yCentre2 = object2.y + 25
+
+    local xOverlap = 0
+    local yOverlap = 0
+
+    -- Resolve X direction first then Y direction second
+
+    -- Assume player can ONLY collide from the right or the left, they can't collide at a corner.
+
+
+    if wasVerticallyAlligned(object1,object2) then
+        if xCentre1 < xCentre2 then
+
+            xOverlap = object1.x + 50 - object2.x
+            object1.x = object1.x - xOverlap
+        else
+            xOverlap = (object2.x + 50) - object1.x
+            object1.x = object1.x + xOverlap
+        end
+
+
+
+    elseif wasHorizontallyAlligned(object1,object2) then
+        if yCentre1 < yCentre2 then
+            yOverlap = (object1.y + 50) - object2.y
+            object1.y = object1.y - yOverlap
+        else
+            yOverlap = object2.y+50 - (object1.y)
+            object1.y = object1.y + yOverlap
+        end
     end
 end
 
@@ -43,9 +77,10 @@ function love.load()
     player = {
         image = love.graphics.newImage("Player.png"),
         x = 700,
-        y = 50,
+        y = 250,
         sx = 1,
-        sy = 1
+        sy = 1,
+        type = "player"
     }
 
     testCentreX = 0
@@ -57,6 +92,7 @@ function love.load()
     -- Tilemap Being Made
 
     map = {
+        {1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -66,14 +102,13 @@ function love.load()
         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1},
-        {1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1},
         {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
     }
 
@@ -99,7 +134,7 @@ function love.update(dt)
     end
 
     if player.y < 1025 then
-        player.y = player.y + 0.5
+        player.y = player.y + 1
     end
 
     -- Every frame check collisions vs the walls in the walls list and the player.
@@ -108,7 +143,6 @@ function love.update(dt)
 
             resolveCollision(player,wallList[i])
 
-            print("Collision detected: " .. dt)
         end
     end
 end
@@ -128,7 +162,7 @@ function love.draw()
     -- Test graphics
 
     love.graphics.setColor(235/255, 225/255, 52/255)
-    
+
     if left then
         love.graphics.rectangle("fill",50,20,50,50)
     elseif right then
